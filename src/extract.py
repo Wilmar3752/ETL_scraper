a4768d94550ba25811d7b618e9bf50e9efc23345
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-23 20:18:09 -0500

    add initial code

---
+++ src/extract.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+def get_data_from_api(num_search_pages: int = 1):
+    url = "http://localhost:8000/product"
+    payload = {
+    "product": "carro"
+    }
+
+    response = requests.post(url, data=json.dumps(payload))
+    return response.json()
