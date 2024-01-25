2c9dd99a00e8f4929bed51497667482019eeba5d
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-01-24 21:32:42 -0500

    add new transformers

--- src/transform.py
+++ src/transform.py
@@ -5,13 +5,14 @@ def transform_json_to_df(json):
     data = data[data['product'].notna()]
     data = get_info_from_name(data)
     data['id'] = [extract_pub_number_from_link(url) for url in data['link']]
-
+    data = clean_locations(data)
     return data
 
 def get_info_from_name(data):
     product_serie = data['product'].str.split(' ')
     data['vehicle_make'] = product_serie.apply(lambda x: x[0])
     data['vehicle_line'] = product_serie.apply(lambda x: x[1])
+    data['kilometraje'] = clean_mileage(data['kilometraje'])
     return data
 
 
@@ -21,3 +22,13 @@ def extract_pub_number_from_link(url):
         return match.group(1)
     else:
         return None
+    
+def clean_mileage(col: pd.Series):
+    mlieage_clean = col.str.replace('.','').str.split(' ').apply(lambda x: x[0]).astype(int)
+    return mlieage_clean
+    
+def clean_locations(df):
+    df['location_city'] = df['locations'].str.split('-').apply(lambda x: x[0]).str.strip()
+    df['location_state'] = df['locations'].str.split('-').apply(lambda x: x[1]).str.strip()
+    df.drop(columns = 'locations', inplace=True)
+    return df
