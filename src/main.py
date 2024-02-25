c0024ad66ca203fcc26ff950942dc408d97b2515
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-02-25 16:42:54 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,21 +1,11 @@
 import logging
-from extract import get_data_from_api
-from transform import transform_json_to_df
-from datetime import datetime
-
-from extract import get_data_from_api
-from transform import transform_json_to_df
-from datetime import datetime
 import boto3
 from botocore.exceptions import ClientError
 
 now = datetime.now().date()
 
-import os
-import boto3
-
 def upload_to_s3(file_name, bucket_name, object_name=None):
-    """Upload a file to an S3 bucket
+    """Upload a file to an S3 bucket using boto3 resource interface
 
     :param file_name: File to upload
     :param bucket_name: Bucket to upload to
@@ -25,28 +15,22 @@ def upload_to_s3(file_name, bucket_name, object_name=None):
     if object_name is None:
         object_name = file_name
 
-    # Verifica si el archivo existe
-    if not os.path.isfile(file_name):
-        print(f"El archivo {file_name} no existe.")
-        return False
-
-    s3_client = boto3.client('s3')
+    s3 = boto3.resource('s3')
     try:
-        response = s3_client.upload_file(file_name, bucket_name, object_name)
-        print(f"Archivo {file_name} subido exitosamente a {bucket_name}/{object_name}")
+        s3.Bucket(bucket_name).upload_file(file_name, object_name)
+        logging.info(f"File {file_name} uploaded to S3 bucket {bucket_name} as {object_name}")
         return True
-    except Exception as e:
-        print(f"Error al subir el archivo: {e}")
+    except ClientError as e:
+        logging.error(f"Error uploading file {file_name} to S3: {e}")
         return False
 
 def main():
     raw_data = get_data_from_api(num_search_pages=1)
     transformed_data = transform_json_to_df(raw_data)
-    transformed_data['_created'] = now 
+    transformed_data['_created'] = now  
     file_name = 'data/raw/data.csv'
-    transformed_data.to_csv(file_name)  
-    # Reemplaza 'your_bucket_name' con el nombre de tu bucket de S3
-    upload_to_s3(transformed_data, bucket_name='scraper-meli', object_name=f'data_{now}')
+    transformed_data.to_csv(file_name)   
+    upload_to_s3(file_name, bucket_name='scraper-meli', object_name=f'data_{now}.csv')
 
 if __name__ == "__main__":
     main()
