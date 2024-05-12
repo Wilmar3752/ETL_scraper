fe7ee4fce18c87a5c29c92b23d1d59d7b7f3eb21
Wilmar Sepulveda Herrera <wilmarsepulvedaherrera@Wilmars-MacBook-Pro.local>
2024-05-12 18:47:11 -0500

    Add all ETL files

--- src/initial_load.py
+++ src/initial_load.py
@@ -1,20 +1,16 @@
-import logging
 from extract import get_data_from_api
 from transform import transform_json_to_df
-from datetime import datetime
 from load import upload_to_s3
-
+from datetime import datetime
 now = datetime.now().date()
 
-
 def main():
-    raw_data = get_data_from_api(num_search_pages=1)
+    raw_data = get_data_from_api(num_search_pages="all")
     transformed_data = transform_json_to_df(raw_data)
     transformed_data['_created'] = now 
-    file_name = 'data/raw/data.csv'
+    file_name = 'data/raw/initial_load.csv'
     transformed_data.to_csv(file_name)  
-    # Reemplaza 'your_bucket_name' con el nombre de tu bucket de S3
-    upload_to_s3(transformed_data, bucket_name='scraper-meli', object_name=f'data_{now}')
+    upload_to_s3(transformed_data, bucket_name='scraper-meli', object_name=f'initial_load')
 
 if __name__ == "__main__":
     main()
