b51d46514ad1c4a73f47facfa38f16bf1e5c2c35
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-11-08 16:03:40 -0500

    change endpoint

--- src/extract.py
+++ src/extract.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 def get_data_from_api(num_search_pages: int = 1, product: str = 'Carros'):
-    url = "http://0.0.0.0:7860/product"
+    url = "https://wilmars-scraper.hf.space/product"
     payload = {
     "product": product,
     "pages":num_search_pages
