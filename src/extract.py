5eac77ebef203d6c98c7b3b41a86b9b488ff9231
Wilmar Sepulveda <wilmar.sepulveda@correounivalle.edu.co>
2024-11-08 15:58:51 -0500

    change etl for car and motos

--- src/extract.py
+++ src/extract.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-def get_data_from_api(num_search_pages: int = 1):
-    url = "https://wilmars-scraper.hf.space/product"
+def get_data_from_api(num_search_pages: int = 1, product: str = 'Carros'):
+    url = "http://0.0.0.0:7860/product"
     payload = {
-    "product": "",
+    "product": product,
     "pages":num_search_pages
     }
 
